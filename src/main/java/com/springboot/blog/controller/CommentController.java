76700f476354ce74d1f67c46abbbc07ac25d2cbf
Onur Haktan <onurhaktan243@gmail.com>
2023-03-21 01:49:57 +0300

    building rest api's for comment resource

---
+++ src/main/java/com/springboot/blog/controller/CommentController.java
@@ -0,0 +1,33 @@
+package com.springboot.blog.controller;
+
+
+import com.springboot.blog.payload.CommentDto;
+import com.springboot.blog.service.CommentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/")
+@RequiredArgsConstructor
+public class CommentController {
+
+    private final CommentService commentService;
+
+    @PostMapping("/posts/{postId}/comments")
+    public ResponseEntity<CommentDto> createComment(@PathVariable(value = "postId") long id,
+                                                    //In order convert the request JSON object into Java object,
+                                                    // we're going to use @RequestBody annotation here
+                                                    @RequestBody CommentDto commentDto){
+        return new ResponseEntity<>(commentService.createComment(id,commentDto), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/posts/{postId}/comments")
+    public List<CommentDto> getCommentsByPostId(@PathVariable(value = "postId") long postId){
+        return commentService.getCommentsByPostId(postId);
+    }
+
+}
---
+++ src/main/java/com/springboot/blog/service/impl/CommentServiceImpl.java
@@ -0,0 +1,70 @@
+package com.springboot.blog.service.impl;
+
+import com.springboot.blog.entity.Comment;
+import com.springboot.blog.entity.Post;
+import com.springboot.blog.exception.ResourceNotFoundException;
+import com.springboot.blog.payload.CommentDto;
+import com.springboot.blog.repository.CommentRepository;
+import com.springboot.blog.repository.PostRepository;
+import com.springboot.blog.service.CommentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class CommentServiceImpl implements CommentService {
+
+    private final CommentRepository commentRepository;
+    private final PostRepository postRepository;
+
+    @Override
+    public CommentDto createComment(long postId, CommentDto commentDto) {
+
+        Comment comment = mapToEntity(commentDto);
+
+        // retrieve post entity by id
+        Post post = postRepository.findById(postId).orElseThrow(
+                () -> new ResourceNotFoundException("Comment", "id", postId)
+        );
+
+        // set post to comment entity
+        comment.setPost(post);
+
+        // comment entity to DB
+        Comment newComment = commentRepository.save(comment);
+
+        return mapToDTO(newComment);
+    }
+
+    @Override
+    public List<CommentDto> getCommentsByPostId(long postId) {
+
+        // retrieve comments by postId
+        List<Comment> comments = commentRepository.findByPostId(postId);
+
+        // convert list of comment entities to list of comment dto's
+        return comments.stream().map(comment -> mapToDTO(comment)).collect(Collectors.toList());
+    }
+
+
+    private CommentDto mapToDTO(Comment comment){
+        CommentDto commentDto = new CommentDto();
+        commentDto.setId(comment.getId());
+        commentDto.setName(comment.getName());
+        commentDto.setEmail(comment.getEmail());
+        commentDto.setBody(comment.getBody());
+        return commentDto;
+    }
+
+    private Comment mapToEntity(CommentDto commentDto){
+        Comment comment = new Comment();
+        comment.setId(comment.getId());
+        comment.setName(commentDto.getName());
+        comment.setEmail(commentDto.getEmail());
+        comment.setBody(commentDto.getBody());
+        return comment;
+    }
+}
