43d4f88b7dbf1b27dcca63ef985dbf2647ff6b01
Onur Haktan <onurhaktan243@gmail.com>
2023-04-06 02:38:00 +0300

    build register rest api

--- src/main/java/com/springboot/blog/service/impl/AuthServiceImpl.java
+++ src/main/java/com/springboot/blog/service/impl/AuthServiceImpl.java
@@ -1,13 +1,24 @@
 package com.springboot.blog.service.impl;
 
+import com.springboot.blog.entity.Role;
+import com.springboot.blog.entity.User;
+import com.springboot.blog.exception.BlogAPIException;
 import com.springboot.blog.payload.LoginDto;
+import com.springboot.blog.payload.RegisterDto;
+import com.springboot.blog.repository.RoleRepository;
+import com.springboot.blog.repository.UserRepository;
 import com.springboot.blog.service.AuthService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
 
 
 @Service
@@ -15,6 +26,10 @@ import org.springframework.stereotype.Service;
 public class AuthServiceImpl implements AuthService {
 
     private final AuthenticationManager authenticationManager;
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
+
 
     @Override
     public String login(LoginDto loginDto) {
@@ -25,5 +40,31 @@ public class AuthServiceImpl implements AuthService {
         SecurityContextHolder.getContext().setAuthentication(authentication);
 
         return "User Logged-in successfully!.";
+    }
+
+    @Override
+    public String register(RegisterDto registerDto) {
+
+        if (userRepository.existsByUsername(registerDto.getUsername())){
+            throw new BlogAPIException(HttpStatus.BAD_REQUEST, "Username is already exists.!");
+        }
+        if (userRepository.existsByEmail(registerDto.getEmail())){
+            throw new BlogAPIException(HttpStatus.BAD_REQUEST, "Email is already exists.!");
+        }
+
+        User user = new User();
+        user.setName(registerDto.getName());
+        user.setUsername(registerDto.getUsername());
+        user.setEmail(registerDto.getEmail());
+        user.setPassword(passwordEncoder.encode(registerDto.getPassword()));
+
+        Set<Role> roles = new HashSet<>();
+        Role userRole = roleRepository.findByName("ROLE_USER").get();
+        roles.add(userRole);
+        user.setRoles(roles);
+
+        userRepository.save(user);
+
+        return "User registered succesfully!";
     }
 }
